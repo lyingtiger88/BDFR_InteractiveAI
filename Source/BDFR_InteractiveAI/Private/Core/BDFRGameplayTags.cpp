ecb5dae98691d4eb017bc4b35798554d37e3e07a
BDFR <66498080+lyingtiger88@users.noreply.github.com>
2026-09-23 10:44:30 +0330

    Add Source/BDFR_InteractiveAI/Private/Core/BDFRGameplayTags.cpp

---
+++ Source/BDFR_InteractiveAI/Private/Core/BDFRGameplayTags.cpp
@@ -0,0 +1,11 @@
+#include "Core/BDFRGameplayTags.h"
+
+namespace BDFRGameplayTags
+{
+    UE_DEFINE_GAMEPLAY_TAG(Locomotion_Gait_Walking, "BDFR.Locomotion.Gait.Walking");
+    UE_DEFINE_GAMEPLAY_TAG(Locomotion_Gait_Running, "BDFR.Locomotion.Gait.Running");
+    UE_DEFINE_GAMEPLAY_TAG(Locomotion_Gait_Sprinting, "BDFR.Locomotion.Gait.Sprinting");
+
+    UE_DEFINE_GAMEPLAY_TAG(Locomotion_Stance_Standing, "BDFR.Locomotion.Stance.Standing");
+    UE_DEFINE_GAMEPLAY_TAG(Locomotion_Stance_Crouching, "BDFR.Locomotion.Stance.Crouching");
+}
