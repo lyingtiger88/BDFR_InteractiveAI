9826b6e6430508911aa19fb908a953d28e2e1e7f
BDFR <66498080+lyingtiger88@users.noreply.github.com>
2026-09-23 10:40:31 +0330

    Add Source/BDFR_InteractiveAI/Private/BDFRInteractiveAI.cpp

---
+++ Source/BDFR_InteractiveAI/Private/BDFRInteractiveAI.cpp
@@ -0,0 +1,13 @@
+#include "BDFRInteractiveAI.h"
+
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_MODULE(FBDFRInteractiveAIModule, BDFR_InteractiveAI)
+
+void FBDFRInteractiveAIModule::StartupModule()
+{
+}
+
+void FBDFRInteractiveAIModule::ShutdownModule()
+{
+}
