fac7c2b02646c2551815dc02185e2b92528a7d7e
BDFR <66498080+lyingtiger88@users.noreply.github.com>
2026-09-23 11:59:08 +0330

    Add Source/BDFR_InteractiveAI/Public/Tracking/BDFRTrackEmitterComponent.h

---
+++ Source/BDFR_InteractiveAI/Public/Tracking/BDFRTrackEmitterComponent.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Tracking/BDFRTrackingTypes.h"
+#include "BDFRTrackEmitterComponent.generated.h"
+
+UCLASS(ClassGroup = (BDFR), meta = (BlueprintSpawnableComponent))
+class BDFR_INTERACTIVEAI_API UBDFRTrackEmitterComponent : public UActorComponent
+{
+    GENERATED_BODY()
+
+public:
+    UBDFRTrackEmitterComponent();
+
+    virtual void BeginPlay() override;
+    virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+    UFUNCTION(BlueprintCallable, Category = "BDFR|Tracking")
+    void SetSurfaceType(EBDFRTrackSurfaceType NewSurfaceType);
+
+    UFUNCTION(BlueprintCallable, Category = "BDFR|Tracking")
+    void SetTrackingEmissionEnabled(bool bEnabled) { bTrackingEmissionEnabled = bEnabled; }
+
+    UFUNCTION(BlueprintCallable, Category = "BDFR|Tracking")
+    void EmitFootprintNow(float StrengthOverride = -1.0f);
+
+    UFUNCTION(BlueprintCallable, Category = "BDFR|Tracking")
+    void EmitScentNow(float StrengthOverride = -1.0f);
+
+protected:
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "BDFR|Tracking")
+    bool bTrackingEmissionEnabled = true;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "BDFR|Tracking|Footprints", meta = (ClampMin = "10.0"))
+    float FootprintSpacingCm = 90.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "BDFR|Tracking|Scent", meta = (ClampMin = "0.1"))
+    float ScentIntervalSeconds = 1.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "BDFR|Tracking|Footprints", meta = (ClampMin = "0.0", ClampMax = "1.0"))
+    float BaseFootprintStrength = 0.75f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "BDFR|Tracking|Scent", meta = (ClampMin = "0.0", ClampMax = "1.0"))
+    float BaseScentStrength = 1.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "BDFR|Tracking")
+    EBDFRTrackSurfaceType SurfaceType = EBDFRTrackSurfaceType::Default;
+
+private:
+    FVector LastFootprintLocation = FVector::ZeroVector;
+    float ScentAccumulator = 0.0f;
+    int32 FootprintSequence = 0;
+    int32 ScentSequence = 0;
+
+    float GetSurfaceFootprintMultiplier() const;
+    float GetSurfaceScentMultiplier() const;
+    void AddSample(EBDFRTrackType TrackType, float Strength, int32 Sequence);
+};
