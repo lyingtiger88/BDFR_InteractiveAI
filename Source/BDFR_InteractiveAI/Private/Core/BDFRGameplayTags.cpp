e3d98fb2822533725ae9d1c90c3974408dc94161
BDFR <66498080+lyingtiger88@users.noreply.github.com>
2026-09-23 10:46:52 +0330

    Export native gameplay tags across modules

--- Source/BDFR_InteractiveAI/Private/Core/BDFRGameplayTags.cpp
+++ Source/BDFR_InteractiveAI/Private/Core/BDFRGameplayTags.cpp
@@ -2,10 +2,10 @@
 
 namespace BDFRGameplayTags
 {
-    UE_DEFINE_GAMEPLAY_TAG(Locomotion_Gait_Walking, "BDFR.Locomotion.Gait.Walking");
-    UE_DEFINE_GAMEPLAY_TAG(Locomotion_Gait_Running, "BDFR.Locomotion.Gait.Running");
-    UE_DEFINE_GAMEPLAY_TAG(Locomotion_Gait_Sprinting, "BDFR.Locomotion.Gait.Sprinting");
+    BDFR_INTERACTIVEAI_API UE_DEFINE_GAMEPLAY_TAG(Locomotion_Gait_Walking, "BDFR.Locomotion.Gait.Walking");
+    BDFR_INTERACTIVEAI_API UE_DEFINE_GAMEPLAY_TAG(Locomotion_Gait_Running, "BDFR.Locomotion.Gait.Running");
+    BDFR_INTERACTIVEAI_API UE_DEFINE_GAMEPLAY_TAG(Locomotion_Gait_Sprinting, "BDFR.Locomotion.Gait.Sprinting");
 
-    UE_DEFINE_GAMEPLAY_TAG(Locomotion_Stance_Standing, "BDFR.Locomotion.Stance.Standing");
-    UE_DEFINE_GAMEPLAY_TAG(Locomotion_Stance_Crouching, "BDFR.Locomotion.Stance.Crouching");
+    BDFR_INTERACTIVEAI_API UE_DEFINE_GAMEPLAY_TAG(Locomotion_Stance_Standing, "BDFR.Locomotion.Stance.Standing");
+    BDFR_INTERACTIVEAI_API UE_DEFINE_GAMEPLAY_TAG(Locomotion_Stance_Crouching, "BDFR.Locomotion.Stance.Crouching");
 }
