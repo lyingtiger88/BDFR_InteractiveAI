0f4197d14a50d2564b9cc18cdbc80c7f2af93738
BDFR <66498080+lyingtiger88@users.noreply.github.com>
2026-09-23 11:08:36 +0330

    Add Source/BDFR_InteractiveAI/Public/Acoustics/BDFRAcousticEventLibrary.h

---
+++ Source/BDFR_InteractiveAI/Public/Acoustics/BDFRAcousticEventLibrary.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Acoustics/BDFRAcousticTypes.h"
+#include "BDFRAcousticEventLibrary.generated.h"
+
+UCLASS()
+class BDFR_INTERACTIVEAI_API UBDFRAcousticEventLibrary : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+
+public:
+    UFUNCTION(BlueprintCallable, Category = "BDFR|Acoustics", meta = (WorldContext = "WorldContextObject"))
+    static void ReportGunshot(
+        UObject* WorldContextObject,
+        AActor* InstigatorActor,
+        FVector Location,
+        bool bSuppressed = false,
+        float HearingRadius = 6000.0f,
+        float ExposureRadius = 900.0f,
+        float PeakExposure = 0.35f);
+
+    UFUNCTION(BlueprintCallable, Category = "BDFR|Acoustics", meta = (WorldContext = "WorldContextObject"))
+    static void ReportExplosion(
+        UObject* WorldContextObject,
+        AActor* InstigatorActor,
+        FVector Location,
+        float HearingRadius = 12000.0f,
+        float BlastExposureRadius = 3500.0f,
+        float PeakExposure = 1.0f,
+        bool bSimulateBlastTravelTime = true,
+        float PropagationSpeedCmPerSecond = 34300.0f);
+
+private:
+    static void ReportAcousticNoise(
+        UObject* WorldContextObject,
+        AActor* InstigatorActor,
+        FVector Location,
+        float Loudness,
+        float HearingRadius,
+        FName Tag);
+
+    static void ApplyRadialExposure(
+        UObject* WorldContextObject,
+        FVector Location,
+        float Radius,
+        float PeakExposure,
+        EBDFRAcousticEventType EventType,
+        bool bUseTravelTime,
+        float PropagationSpeedCmPerSecond);
+};
