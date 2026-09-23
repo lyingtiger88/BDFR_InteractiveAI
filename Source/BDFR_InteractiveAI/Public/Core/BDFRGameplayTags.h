3a32792657aaeb62dcc3ced6915a653cab91b716
BDFR <66498080+lyingtiger88@users.noreply.github.com>
2026-09-23 10:44:28 +0330

    Add Source/BDFR_InteractiveAI/Public/Core/BDFRGameplayTags.h

---
+++ Source/BDFR_InteractiveAI/Public/Core/BDFRGameplayTags.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace BDFRGameplayTags
+{
+    BDFR_INTERACTIVEAI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Locomotion_Gait_Walking);
+    BDFR_INTERACTIVEAI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Locomotion_Gait_Running);
+    BDFR_INTERACTIVEAI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Locomotion_Gait_Sprinting);
+
+    BDFR_INTERACTIVEAI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Locomotion_Stance_Standing);
+    BDFR_INTERACTIVEAI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Locomotion_Stance_Crouching);
+}
