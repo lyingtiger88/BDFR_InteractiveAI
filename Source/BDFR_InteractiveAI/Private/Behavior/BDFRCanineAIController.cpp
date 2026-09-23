229eac8293b8c32aceb63faf393d297549f43f4b
BDFR <66498080+lyingtiger88@users.noreply.github.com>
2026-09-23 12:18:04 +0330

    Attach attention and animation presentation to canine AI

--- Source/BDFR_InteractiveAI/Private/Behavior/BDFRCanineAIController.cpp
+++ Source/BDFR_InteractiveAI/Private/Behavior/BDFRCanineAIController.cpp
@@ -1,9 +1,12 @@
 #include "Behavior/BDFRCanineAIController.h"
 
+#include "Canine/BDFRCanineAttentionComponent.h"
 #include "Tracking/BDFRCanineTrackingComponent.h"
 
 ABDFRCanineAIController::ABDFRCanineAIController()
 {
     CanineTrackingComponent =
         CreateDefaultSubobject<UBDFRCanineTrackingComponent>(TEXT("BDFRCanineTracking"));
+    CanineAttentionComponent =
+        CreateDefaultSubobject<UBDFRCanineAttentionComponent>(TEXT("BDFRCanineAttention"));
 }
